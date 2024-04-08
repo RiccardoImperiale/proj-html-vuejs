aac808f9628536510a212a76450a4224bc6c422a
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-08 11:17:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'AppHeader',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<template>
+  header
+</template>
+
+<style></style>
